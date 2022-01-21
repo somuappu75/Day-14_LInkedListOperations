721bf38f610234c7535032f3e352f7bd677d9d73
Lenovo <somusp75@gmail.com>
2022-01-21 21:04:05 +0530

    Uc-1 Insertion at Last

---
+++ Ds_LInkedListOperations/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ds_LInkedListOperations
+{
+    class Node
+    {
+        // Node Craetion
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
