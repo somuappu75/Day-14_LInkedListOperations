789f753851896fe13e4aa26e0852c10e0529727e
Lenovo <somusp75@gmail.com>
2022-01-21 20:52:03 +0530

    Add project files.

---
+++ Ds_LInkedListOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ds_LInkedListOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
