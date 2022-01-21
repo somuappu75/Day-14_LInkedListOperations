0a8ca6a9f19b3a81f08ec7a0995cf8c473251047
Lenovo <somusp75@gmail.com>
2022-01-21 21:15:24 +0530

    Uc-2 INsert AT Top

--- Ds_LInkedListOperations/Program.cs
+++ Ds_LInkedListOperations/Program.cs
@@ -6,16 +6,27 @@ namespace Ds_LInkedListOperations
     {
         static void Main(string[] args)
         {
-            //uc-1
+            ////uc-1
+            //Operations obj = new Operations();
+            ////Insertion Operation
+            //obj.InsertLast(56);
+            //obj.InsertLast(30);
+            //obj.InsertLast(70);
+            ////Display Operation
+            //Console.WriteLine("--------insertion at Last-------");
+            //obj.Display();
+            //// Console.WriteLine("Hello World!");
+
+            //UseCase 2
             Operations obj = new Operations();
             //Insertion Operation
-            obj.InsertLast(56);
-            obj.InsertLast(30);
-            obj.InsertLast(70);
+            obj.InsertFront(56);
+            obj.InsertFront(30);
+            obj.InsertFront(70);
             //Display Operation
-            Console.WriteLine("--------insertion at Last-------");
+            Console.WriteLine("------Insertion at Top------");
             obj.Display();
-            // Console.WriteLine("Hello World!");
         }
     }
-}
+    }
+
